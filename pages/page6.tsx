a489e12cd07f1219eb54b1475b4235285a322f5b
Digi | Toxic <DigiToxic55@gmail.com>
2023-09-11 15:57:23 -0400

    official

--- pages/page6.tsx
+++ pages/page6.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 function page6() {
   return (
@@ -6,9 +7,9 @@ function page6() {
 
       <div className="flex flex-col justify-center items-center" id="Try">
         <img className="w-[900px] max-[700px]:w-[600px]" src="/DeVinciAI.png" alt="" />
-        <div className="border-2 border-[#ff4d00] rounded-full mt-4 cursor-pointer shadow-md shadow-[#ff4d00] hover:translate-y-2 hover:shadow-none">
+        <Link href="https://calendly.com/d/4tg-bwp-db5/ai-automation-web" className="border-2 border-[#ff4d00] rounded-full mt-4 cursor-pointer shadow-md shadow-[#ff4d00] hover:translate-y-2 hover:shadow-none">
           <p className="text-[#ff4d00] font-bold text-3xl rounded-full p-2 hover:text-white hover:bg-[#ff4d00] active:bg-[#8c2a00] max-[500px]:text-2xl">TRY IT FOR FREE NOW!</p>
-        </div>
+        </Link>
       </div>
     </div>
   );
